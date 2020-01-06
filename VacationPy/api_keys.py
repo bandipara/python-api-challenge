da74e37397ff3aacbb7411c929aeef55414e4002
bandipara <b.andipara@hotmail.com>
2020-01-05 21:56:51 -0500

    gkey removed

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "63674108a01d1639c5d62d72793ab4be"
 
 # Google API Key
-g_key = "AIzaSyDiw4WzBz-FysjldTmvl5YACHYlvZpyYd0"
+g_key = "Your Key Here!"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "c55e1c9f1c3f28bab994931b713e9682"
 
 # Google API Key
-g_key = "AIzaSyDiw4WzBz-FysjldTmvl5YACHYlvZpyYd0"
+g_key = "Your Key Here!"
